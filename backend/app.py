192c011d0fe67dc60d844d626efc7f629b16e605
Hoang Ho Minh <minhhoangho99@gmail.com>
2024-03-05 18:01:06 +0700

    Add camera

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ detector = DetectionUtil(os.path.join("./models", "yolov8m.pt"))
 
 stream = CamGear(source='https://youtu.be/dQw4w9WgXcQ', stream_mode = True, logging=True).start() # YouTube Video URL as input
 
-def generate_frames(video_id="FQ1k_LCa0I4"):
+def generate_frames(video_id="Lc8oxWnjUzI"):
     video_url = f"https://www.youtube.com/watch?v={video_id}"
     # yt = YouTube(video_url)
     # stream = yt.streams.get_highest_resolution()
