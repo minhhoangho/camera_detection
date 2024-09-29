f3b4c985dd1a525c35e64e0595497e03dc82586c
Hoang Ho Minh <minhhoangho99@gmail.com>
2024-09-29 22:10:55 +0700

    Update UI sidebar

--- frontend/src/containers/HomePage/Sidebar/Sidebar.tsx
+++ frontend/src/containers/HomePage/Sidebar/Sidebar.tsx
@@ -27,6 +27,7 @@ import { Iconify } from '../../../components/Iconify';
 import { Scrollbar } from '../../../components/Scrollbar';
 import { PathName } from '../../../constants/routes';
 import Spinner from '../../../components/Spinner';
+import styles from './Sidebar.module.scss';
 
 type Props = {
   onClose: () => void;
@@ -38,26 +39,25 @@ export function Sidebar({ open, onClose }: Props): React.ReactElement {
   const pathname = router.pathname;
   const [keyword, setKeyword] = React.useState<string | null>('');
 
-  const {
-    data,
-    fetchNextPage,
-    isLoading,
-    isFetching,
-  } = useInfiniteQuery<ListViewPointPaginateResponse>({
-    queryKey: ['getListViewPointPaginate', keyword],
-    queryFn: ({ pageParam }) => listViewPointsPaginate({
-      keyword: keyword ?? '',
-      pagination: {
-        offset: pageParam?.offset ?? 0,
-        limit: DEFAULT_PAGINATION_PARAMS.limit,
+  const { data, fetchNextPage, isLoading, isFetching } =
+    useInfiniteQuery<ListViewPointPaginateResponse>({
+      queryKey: ['getListViewPointPaginate', keyword],
+      queryFn: ({ pageParam }) =>
+        listViewPointsPaginate({
+          keyword: keyword ?? '',
+          pagination: {
+            offset: pageParam?.offset ?? 0,
+            limit: DEFAULT_PAGINATION_PARAMS.limit,
+          },
+        }),
+      getNextPageParam: (lastPage: ListViewPointPaginateResponse) => {
+        const _offset =
+          lastPage?.pagination.offset + DEFAULT_PAGINATION_PARAMS.limit;
+        return _offset < lastPage?.pagination.total
+          ? { offset: _offset }
+          : undefined;
       },
-    }),
-    getNextPageParam: (lastPage: ListViewPointPaginateResponse) => {
-       const _offset = lastPage?.pagination.offset + DEFAULT_PAGINATION_PARAMS.limit;
-       return _offset < lastPage?.pagination.total ? { offset: _offset } : undefined;
-    },
-  })
-
+    });
 
   useEffect(() => {
     if (open) {
@@ -77,7 +77,7 @@ export function Sidebar({ open, onClose }: Props): React.ReactElement {
         scrollHeight: number;
         clientHeight: number;
       } = event.target;
-      if ((scrollTop + clientHeight >= scrollHeight - 5) && !isFetching) {
+      if (scrollTop + clientHeight >= scrollHeight - 5 && !isFetching) {
         fetchNextPage();
       }
     }
@@ -85,8 +85,15 @@ export function Sidebar({ open, onClose }: Props): React.ReactElement {
 
   const renderResultItem = (item: ViewPointData) => {
     return (
-      <Card sx={{ maxWidth: 500 }} className="mt-2" key={item.id}>
-        <CardActionArea>
+      <Card
+        sx={{ maxWidth: 500 }}
+        className="my-4 px-4"
+        key={item.id}
+        style={{
+          background: 'transparent',
+        }}
+      >
+        <CardActionArea className={styles['custom-card-border']}>
           <Skeleton variant="rectangular" height={140} animation={false} />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
@@ -152,9 +159,13 @@ export function Sidebar({ open, onClose }: Props): React.ReactElement {
               <React.Fragment key={_index}>
                 {page.data.map((item) => renderResultItem(item))}
               </React.Fragment>
-            )
+            );
           })}
-        {(isLoading || isFetching) && (<div className="w-100 flex justify-center"><Spinner /></div>)}
+        {(isLoading || isFetching) && (
+          <div className="w-100 flex justify-center">
+            <Spinner />
+          </div>
+        )}
       </Scrollbar>
     </>
   );
