075a3fb6f95fffd186de772c5714d5b7f33c6a78
Hoang Ho Minh <minhhoangho99@gmail.com>
2024-10-07 08:06:54 +0700

    update realtime cam

--- frontend/src/containers/GisMap/components/RealtimeCamera.tsx
+++ frontend/src/containers/GisMap/components/RealtimeCamera.tsx
@@ -1,15 +1,16 @@
-import { Box, Card, CardHeader } from '@mui/material';
+import { Box, Card } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import * as React from 'react';
+import Typography from '@mui/material/Typography';
 import { API_BASE_URL } from '../../../constants';
 import { ViewPointCameraData, ViewPointData } from '../models';
-import Typography from '@mui/material/Typography';
 
 type RealtimeCameraProps = {
   viewPoint?: ViewPointData;
   viewPointCamera: ViewPointCameraData;
   setShowRealtimeCamera?: (val: boolean) => void;
 };
+
 export function RealtimeCamera({
   viewPointCamera,
   viewPoint,
@@ -19,21 +20,21 @@ export function RealtimeCamera({
   return (
     <Box>
       <div>
-        {
-          setShowRealtimeCamera && <ArrowBackIcon
+        {setShowRealtimeCamera && (
+          <ArrowBackIcon
             className="cursor-pointer"
             onClick={() => setShowRealtimeCamera(false)}
           />
-        }
+        )}
       </div>
       <Card>
         <div className="px-5 py-1">
-          <Typography variant="h6" >{title}</Typography>
+          <Typography variant="h6">{title}</Typography>
         </div>
         {/*<CardHeader title={title} subheader={viewPoint?.name} />*/}
         <Box sx={{ p: 2 }}>
           <img
-            src={`${API_BASE_URL}/detector/video/realtime?type=${viewPointCamera.cameraSource}&uri=${viewPointCamera.cameraUri}`}
+            src={`${API_BASE_URL}/detector/video/realtime?type=${viewPointCamera.cameraSource}&uri=${viewPointCamera.cameraUri}&cam_id=${viewPointCamera.id}`}
             alt="video"
           />
         </Box>
