34f209fa54c2ded158c02f6848a30b032e8acfb7
Hoang Ho Minh <minhhoangho99@gmail.com>
2024-11-17 15:28:48 +0700

    update alow cors

--- backend/src/settings/environments/local.py
+++ backend/src/settings/environments/local.py
@@ -6,6 +6,9 @@ ALLOWED_HOST = [
     "0.0.0.0",  # noqa: S104
     "127.0.0.1",
     "[::1]",
+    "https://camera-detection.vercel.app",
+    "https://city-camera.vercel.app/home",
+    "https://accurately-crack-chamois.ngrok-free.app"
 ]
 #
 # SECURE_CROSS_ORIGIN_OPENER_POLICY = "cross-site"
@@ -15,4 +18,8 @@ SECURE_REFERRER_POLICY =  None
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
     "http://localhost:3001",
+    "https://camera-detection.vercel.app",
+    "https://city-camera.vercel.app",
+    "https://*.vercel.app",
+    "https://accurately-crack-chamois.ngrok-free.app"
 ]
