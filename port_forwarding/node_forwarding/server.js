3628cfb9a731a48f782a0d26f431daf5f53c01d3
Hoang Ho Minh <minhhoangho99@gmail.com>
2024-12-17 19:55:51 +0700

    Add forward server

--- port_forwarding/node_forwarding/server.js
+++ port_forwarding/node_forwarding/server.js
@@ -6,6 +6,18 @@ const proxy = httpProxy.createProxyServer({});
 
 // Create a server that forwards requests
 const server = http.createServer((req, res) => {
+        // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
     // Forward requests to the target server
     const target = 'http://localhost:8090'; // Change to your target port
     proxy.web(req, res, { target: target }, (error) => {
@@ -20,3 +32,4 @@ const PORT = 8080;
 server.listen(PORT, () => {
     console.log(`Proxy server is running at http://localhost:${PORT}`);
 });
+
